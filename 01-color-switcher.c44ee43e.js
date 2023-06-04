b00f455bb5570bf9adc5852df1edf3b8ad10bb7c
OlgaBilynska <122179645+OlgaBilynska@users.noreply.github.com>
2023-06-04 07:58:31 +0000

    Deploying to gh-pages from @ OlgaBilynska/goit-js-hw-09@25c065aa2f6dd8b3b64ac397690c2dfff1b888b2 🚀

---
+++ 01-color-switcher.c44ee43e.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");let d=null;function o(){n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(function(){d=setInterval(o,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.c44ee43e.js.map
